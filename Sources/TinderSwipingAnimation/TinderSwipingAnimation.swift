3b16c907fa5f0dbdb89576927d47f2d5d0abade8
omarthamri <omarthamri4@gmail.com>
2023-11-26 00:43:53 +0100

    implement animation

--- Sources/TinderSwipingAnimation/TinderSwipingAnimation.swift
+++ Sources/TinderSwipingAnimation/TinderSwipingAnimation.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 public struct TinderSwipingAnimation: View {
     var cards: [CardModel]
-    var body: some View {
+    public var body: some View {
         ZStack{
             Color.black.opacity(0.05)
                 .ignoresSafeArea(edges: .all)
