b025edc89d7f25b3dad8e6c71c9d839eb68bf51d
omarthamri <omarthamri4@gmail.com>
2023-11-26 00:24:40 +0100

    implement animation

--- Package.swift
+++ Package.swift
@@ -5,19 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "TinderSwipingAnimation",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "TinderSwipingAnimation",
             targets: ["TinderSwipingAnimation"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TinderSwipingAnimation"),
-        .testTarget(
-            name: "TinderSwipingAnimationTests",
-            dependencies: ["TinderSwipingAnimation"]),
     ]
 )
