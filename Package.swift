11aaada6eff9a2b173b08bce19938f5bab571c64
omarthamri <omarthamri4@gmail.com>
2023-11-26 04:20:21 +0100

    fix bug

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TinderSwipingAnimation",
-    defaultLocalization: "en",
+    defaultLocalization: "fr",
     platforms: [
         .iOS(.v16)
     ],
