e41dc62e8657cae24b9ca9493ad5e39559bb2998
omarthamri <omarthamri4@gmail.com>
2023-11-30 16:26:49 +0100

    Fix bug in buttons

--- Sources/TinderSwipingAnimation/Models/ButtonModel.swift
+++ Sources/TinderSwipingAnimation/Models/ButtonModel.swift
@@ -10,13 +10,14 @@ import SwiftUI
 public struct ButtonModel {
     var image: Image
     var color: Color
-    var action: ButtonAction = .none
-    public init(image: Image, color: Color) {
+    var action: ButtonAction
+    public init(image: Image, color: Color, action: ButtonAction = .none) {
         self.image = image
         self.color = color
+        self.action = action
     }
     
-    enum ButtonAction {
+    public enum ButtonAction {
         case none
         case goRight
         case goLeft
